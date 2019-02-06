c4ada2ef8cc018cd791ae237cdc60b3b4df813a2
alumno <alumno>
2019-02-06 19:04:57 -0300

    Ejercicio 4 resubida

--- 1-EntradaSalida/jsEntradaSalida-4.js
+++ 1-EntradaSalida/jsEntradaSalida-4.js
@@ -2,6 +2,7 @@
 	Debemos lograr tomar un dato por 'PROMPT' 
 	y lo muestro por 'getElementById' al presionar el botón 'mostrar'
 */
+//Ya lo habia hecho pero tengo que resubirlo a git para la clase
 function mostrar()
 {
 	var nombre;	
