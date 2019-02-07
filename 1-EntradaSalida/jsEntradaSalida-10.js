7ed214932a0c00dfe0b4587c6e83e0c259135b18
alumno <alumno>
2019-02-06 21:50:55 -0300

    Ejercicios 9 y 10

--- 1-EntradaSalida/jsEntradaSalida-10.js
+++ 1-EntradaSalida/jsEntradaSalida-10.js
@@ -4,5 +4,10 @@ mostrar el importe con un Descuento del 25 %
 en el cuadro de texto "RESULTADO"*/
 function mostrarAumento()
 {
-	
+	var Importe;
+
+	Importe = importe.value;
+	Importe = parseInt(Importe) * 0.75;
+
+	resultado.value = Importe;
 }
--- 1-EntradaSalida/jsEntradaSalida-9.js
+++ 1-EntradaSalida/jsEntradaSalida-9.js
@@ -2,8 +2,29 @@
 transformarlo a entero (parseInt), luego
 mostrar el importe con un aumento del 10 %
 en el cuadro de texto "RESULTADO".*/
+/*
+APUNTE
+
+Descuento:
+Sueldo = 100
+Descuento = Sueldo*(porcentaje a cambiar)/100
+Sueldo = Sueldo - descuento 
+Sueldo * (1-0.1)
+Sueldo * 0.9
+
+Aumento:
+Sueldo = 100
+Aumento = Sueldo + Sueldo * (porcentaje)/100
+Sueldo * (1+0.1)
+Sueldo * 1.1
+*/
 function mostrarAumento()
 {
+	var Sueldo;
 
+	Sueldo = sueldo.value;
+	Sueldo = parseInt(Sueldo) * 1.1;
+
+	resultado.value = Sueldo;
 	
 }
