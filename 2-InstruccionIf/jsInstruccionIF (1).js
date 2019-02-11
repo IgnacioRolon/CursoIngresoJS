4fdd20afbfbd8179c35aa99f4c834f475688fdb4
alumno <alumno>
2019-02-11 19:27:38 -0300

    Ejercicio 1 actualizado

--- 2-InstruccionIf/jsInstruccionIF (1).js
+++ 2-InstruccionIf/jsInstruccionIF (1).js
@@ -3,8 +3,23 @@ function mostrar()
 //tomo la edad  
 var Edad;
 
+
 Edad = edad.value;
+//Edad = parseInt(Edad);
+/*Si se parsea la edad, se compara con el valor numerico
+En caso contrario, se compara con el valor en forma 
+de string.
+
+Si estuviese parseado el if sería:
+
 if (Edad == 15) 
+{
+	alert("niña bonita");
+}
+
+*/
+
+if (Edad == "15") 
 {
 	alert("niña bonita");
 }
