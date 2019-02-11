45afa6ef63b26085b834bbae98ecf1d9b1e2cd9f
alumno <alumno>
2019-02-11 20:02:22 -0300

    Ejercicios 2 y 3

--- 2-InstruccionIf/jsInstruccionIF (2).js
+++ 2-InstruccionIf/jsInstruccionIF (2).js
@@ -1,7 +1,23 @@
 function mostrar()
 {
 //tomo la edad  
+var laEdad;
+laEdad = edad.value;
+laEdad = parseInt(laEdad);
+
+if (laEdad >= 18) 
+{
+	alert("La persona es mayor de edad.")
+}
+
+/* Forma alternativa
+
+if (laEdad > 17) 
+{
+	alert("La persona es mayor de edad.")
+}
 
 
+*/
 
 }//FIN DE LA FUNCIÓN
--- 2-InstruccionIf/jsInstruccionIF (3).js
+++ 2-InstruccionIf/jsInstruccionIF (3).js
@@ -1,6 +1,33 @@
 function mostrar()
 {
-//tomo la edad  
+	//tomo la edad  
+	var laEdad;
+	laEdad = edad.value;
+	laEdad = parseInt(laEdad);
 
+	if (laEdad >= 18) 
+	{
+		alert("La persona es mayor de edad.")
+	} 
+	if (laEdad <= 17)
+	{
+		alert("La persona es menor de edad.")
+	}
+
+	/*
+
+	if (laEdad >= 18) 
+	{
+		alert("La persona es mayor de edad.")
+	} 
+	else
+	{
+		alert("La persona es menor de edad.")
+	}
+
+	Se puede realizar con else, pero eso incluiría
+	otras opciones que no son validas, como palabras.
+	
+	*/
 
 }//FIN DE LA FUNCIÓN
