b1528802b352bd551440a23574d27a320b96b0ef
alumno <alumno>
2019-02-11 22:14:16 -0300

    Ejercicio 9

--- 2-InstruccionIf/jsInstruccionIF (9).js
+++ 2-InstruccionIf/jsInstruccionIF (9).js
@@ -1,6 +1,7 @@
 function mostrar()
 {
-	//Genero el número RANDOM entre 1 y 10 
-	
-
+	//Genero el número RANDOM entre 1 y 10
+	var randomnum; 	
+	randomnum = Math.floor(Math.random() * 10) + 1; 
+	alert(randomnum)
 }//FIN DE LA FUNCIÓN
