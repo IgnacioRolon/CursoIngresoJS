5bfe41567634e39057dc1b30113fee7a4f48f313
Ignacio Rolon <ignirolon@gmail.com>
2019-02-13 00:03:23 -0300

    Ejercicios parcial y tps

--- 9-Parciales/2018_1Cuatri_Parcial_fuentes/cuatro.js
+++ 9-Parciales/2018_1Cuatri_Parcial_fuentes/cuatro.js
@@ -1,4 +1,41 @@
 function mostrar()
 {
+	
+	
+	var numero1;
+	var numero2;
+	var resultado;
 
+	numero1 = prompt("Ingrese el primer numero");
+	numero2 = prompt("Ingrese el segundo numero");
+
+	if(numero1 == numero2) //RECORDAR PONER doble ==
+	{
+		resultado = numero1 + numero2;
+		alert(resultado);
+	}
+	else
+	{
+		numero1 = parseInt(numero1);
+		numero2 = parseInt(numero2);
+		if(numero1 > numero2)
+		{
+			resultado = numero1 - numero2;
+			alert(resultado);
+		}
+		else
+		{
+			resultado = numero1 + numero2;
+			if (resultado > 10)
+			{
+				alert("La suma es " + resultado + " y supero el 10");
+			}
+			else
+			{
+				alert(resultado);
+			}
+		}	
+	}
+	
+	
 }
--- 9-Parciales/2018_1Cuatri_Recu_fuentes/uno.js
+++ 9-Parciales/2018_1Cuatri_Recu_fuentes/uno.js
@@ -2,4 +2,13 @@
 function mostrar()
 {
 
+	var ancho;
+	var largo;
+	var perimetro;
+	ancho = prompt("Ingrese el ancho del rectangulo");
+	largo = prompt("Ingrese el largo del rectangulo");
+
+	perimetro = ancho * 2 + largo * 2;
+	alert("El perimetro es de: " + perimetro);
+
 }
