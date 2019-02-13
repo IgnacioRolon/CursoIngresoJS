a1ccfe4c46c23809c0ed93b891f78d92b0718a1e
alumno <alumno>
2019-02-13 19:46:11 -0300

    Ejercicio 3 Parcial

--- 9-Parciales/2018_1Cuatri_Parcial_fuentes/cuatro.js
+++ 9-Parciales/2018_1Cuatri_Parcial_fuentes/cuatro.js
@@ -2,6 +2,7 @@ function mostrar()
 {
 	
 	
+	/*
 	var numero1;
 	var numero2;
 	var resultado;
@@ -36,6 +37,50 @@ function mostrar()
 			}
 		}	
 	}
+	Ejercicio parcial original
+	*/
 	
+	var cantidadProductos;
+	var precioProductos;
+	var descuento;
+	var formaDePago;
+	var precioFinal;
+	var recargo;
 	
+	descuento = 0;
+	recargo = 0;
+	cantidadProductos = prompt("Ingrese la cantidad de productos");
+	precioProductos = prompt("Ingrese el precio por unidad");
+	formaDePago = prompt("Indique el metodo de pago ingresando el numero correspondiente: 1- Efectivo, 2-Tarjeta")
+	
+	cantidadProductos = parseInt(cantidadProductos);
+	precioProductos = parseInt(precioProductos);
+	formaDePago = parseInt(formaDePago);
+
+	precioFinal = precioProductos * cantidadProductos;
+
+	if(cantidadProductos > 2){
+		descuento = descuento + 0.10;
+	}
+
+	if(precioFinal > 2000){
+		descuento = descuento + 0.15;
+	}
+
+	if (formaDePago == 2){
+		recargo = 0.10;
+	}
+
+	precioFinal = precioFinal - precioFinal * descuento + precioFinal * recargo;
+
+	alert("El precio final es de: " + precioFinal);
 }
+
+/*
+al realizar una compra, si compra de mas 
+de dos producto se realiza un descuento
+del 10% y si supera los $2000 se agrega
+un descuento adicional de 15%, si el pago
+es con tarjeta y no efectivo al final
+se le agrega un 10% de recargo.
+*/
