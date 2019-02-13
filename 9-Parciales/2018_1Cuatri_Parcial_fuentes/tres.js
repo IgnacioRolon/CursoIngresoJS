40992d674347138dea9b379deb539ed47cb8ecef
alumno <alumno>
2019-02-13 19:16:24 -0300

    Ejercicio 1 Parcial

--- 9-Parciales/2018_1Cuatri_Parcial_fuentes/tres.js
+++ 9-Parciales/2018_1Cuatri_Parcial_fuentes/tres.js
@@ -1,11 +1,13 @@
 function mostrar()
 {
 	var precioinicial;
-	precioinicial = prompt("Ingrese el precio");
 	var porcentaje;
+	var precio;
+
+	precioinicial = prompt("Ingrese el precio");	
 	porcentaje = prompt("Ingrese el porcentaje de descuento");
 
-	var precio;
+	
 	porcentaje = parseInt(porcentaje) / 100;
 	precio = parseInt(precioinicial) * porcentaje;
 	precio = precioinicial - precio;
--- 9-Parciales/2018_1Cuatri_Parcial_fuentes/uno.js
+++ 9-Parciales/2018_1Cuatri_Parcial_fuentes/uno.js
@@ -1,14 +1,36 @@
+/* alert("Ustedes son xxxx y xxxx, sus edades son xx y xx, 
+y la suma de sus edades es xx")
+
+*/
 
 function mostrar()
 {
 
-	var ancho;
+	/* var ancho;
 	var largo;
 	var perimetro;
 	ancho = prompt("Ingrese el ancho del rectangulo");
 	largo = prompt("Ingrese el largo del rectangulo");
 
 	perimetro = ancho * 2 + largo * 2;
-	alert("El perimetro es de: " + perimetro);
+	alert("El perimetro es de: " + perimetro); 
+	Ejercicio Parcial original
+	*/
 
+	var nombreUno;
+	var nombreDos;
+	var edadUno;
+	var edadDos;
+	var sumaEdades;
+
+	nombreUno = prompt("Ingrese el nombre del primero");
+	nombreDos = prompt("Ingrese el nombre del segundo");
+	edadUno = prompt("Ingrese la edad del primero");
+	edadDos = prompt("Ingrese la edad del segundo");
+
+	edadUno = parseInt(edadUno);
+	edadDos = parseInt(edadDos);
+	sumaEdades = edadUno + edadDos;
+
+	alert("Ustedes son " + nombreUno + " y " + nombreDos + ", sus edades son " + edadUno + " y " + edadDos + ", y la suma de sus edades es " + sumaEdades);
 }
