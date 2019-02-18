6fc230b5f2c42b656cd97509b3aab2a40d822726
alumno <alumno>
2019-02-18 20:26:19 -0300

    Ejercicios Switch

--- 3-InstruccionSwitch/jsInstruccionSwitch(5).js
+++ 3-InstruccionSwitch/jsInstruccionSwitch(5).js
@@ -2,11 +2,22 @@ function mostrar()
 {
 //tomo la edad  
 var laHora = document.getElementById('hora').value;
-
+laHora = parseInt(laHora);
+var mensaje = "";
 //alert (laHora);
-	
-	
-
+switch(laHora)
+{
+	case 7:
+	case 8:
+	case 9:
+	case 10:
+	case 11:
+		mensaje = "Es de mañana";
+	break;
+}
+if(mensaje != ""){
+	alert(mensaje);
+}
 
 
 }//FIN DE LA FUNCIÓN
