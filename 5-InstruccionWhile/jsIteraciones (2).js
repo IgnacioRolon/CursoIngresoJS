3a36681751411ed27232bd02b5a31892128024d2
alumno <alumno>
2019-02-18 21:58:06 -0300

    Ejercicios While

--- 5-InstruccionWhile/jsIteraciones (2).js
+++ 5-InstruccionWhile/jsIteraciones (2).js
@@ -1,6 +1,11 @@
 function mostrar()
 {
-	alert('iteración while');
-
+	var contador = 0;
+	//var contador = 10;
+	contador = contador + 10;
+	while(contador > 0){				
+		console.log(contador);
+		contador = contador - 1;
+	}
 
 }//FIN DE LA FUNCIÓN
